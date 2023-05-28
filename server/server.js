b0d557b8ab6d892f4e87d1fc8fe5f42cc94d9370
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2023-05-27 23:29:37 -0700

    connecting Mongo Atlas

--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 8000;
-    
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello World" });
-});
+require('dotenv').config();
+
+// config mongoose
+require('./config/mongoose.config');
+
+//config express
+app.use(cors( {
+    credentials: true,
+    origin:'http://localhost:3000'
+}))
+
+//POST method
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+require('./routes/person.routes')(app);
+
+//port
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
