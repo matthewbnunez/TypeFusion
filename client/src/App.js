89b3b1145017db640244e26b7fbd4e10c64afb35
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2023-05-30 07:35:23 -0700

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
-import React from 'react';
-import Main from './views/Main';
+import "./App.css";
+import React from "react";
+import Main from "./views/Main";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <routes>
+        <route element={<Main />} path="/" />
+      </routes>
     </div>
   );
 }
